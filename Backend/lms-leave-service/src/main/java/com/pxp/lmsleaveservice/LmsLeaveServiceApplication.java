248e9de481d7caafa567191487b649ef98801a00
Sarthak Mehta <sarthakmehta649@gmail.com>
2023-03-11 17:37:39 +0530

    first microservice added : leave service

---
+++ Backend/lms-leave-service/src/main/java/com/pxp/lmsleaveservice/LmsLeaveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pxp.lmsleaveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsLeaveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsLeaveServiceApplication.class, args);
+	}
+
+}
