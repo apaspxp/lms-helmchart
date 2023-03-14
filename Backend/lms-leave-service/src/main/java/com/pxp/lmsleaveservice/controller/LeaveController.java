d011a17f2eb9440a45731bfdd334f0955fc95c4c
rajesh1420621 <panigrahirajesh578@gmail.com>
2023-03-14 23:55:56 +0530

    LMS-4: Create an endpoint to upload holiday calendar using excel.

---
+++ Backend/lms-leave-service/src/main/java/com/pxp/lmsleaveservice/controller/LeaveController.java
@@ -0,0 +1,26 @@
+package com.pxp.lmsleaveservice.controller;
+
+import com.pxp.lmsleaveservice.service.HolidayCalendarService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping(value = "/api")
+@Slf4j
+public class LeaveController {
+
+    @Autowired
+    private HolidayCalendarService holidayCalendarService;
+
+    @RequestMapping(value = "uploadHolidayCalendar/{city}/{year}", method = RequestMethod.POST)
+    public ResponseEntity<String> uploadHolidayCalendar(@RequestBody MultipartFile file, @PathVariable String city, @PathVariable int year){
+        log.info("Entered into method uploadHolidayCalendar()");
+        return new ResponseEntity<>(holidayCalendarService.saveHolidayCalender(file,city,year), HttpStatus.OK);
+    }
+}
