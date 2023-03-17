6e57c0dfe5f80ada896e2866f5e8a9b2dc1c36a6
Sarthak Mehta <sarthakmehta649@gmail.com>
2023-03-17 10:58:31 +0530

    removed admin service

--- Backend/admin-service/src/main/java/com/lms/adminservice/service/interfaces/IHolidayCalendarService.java
+++
@@ -1,9 +0,0 @@
-package com.lms.adminservice.service.interfaces;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface IHolidayCalendarService {
-
-    public String saveHolidayCalender(MultipartFile file, String city, int year);
-
-}
