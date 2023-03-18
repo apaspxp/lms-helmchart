18c500de64be2bbc4681c04a275f07d4636c2ff5
Sarthak Mehta <sarthakmehta649@gmail.com>
2023-03-18 10:03:17 +0530

    module name changed

--- Backend/notification-service/src/main/java/com/lms/notificationservice/LmsNotificationServiceApplication.java
+++ Backend/notification-service/src/main/java/com/lms/notificationservice/LmsNotificationServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationServiceApplication {
+public class LmsNotificationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
+		SpringApplication.run(LmsNotificationServiceApplication.class, args);
 	}
 
 }
