4467872d01ba2b6564b6549050bb2cdc5f2201de
rajesh1420621 <panigrahirajesh578@gmail.com>
2023-03-27 22:35:31 +0530

    LMS-7: liquibase integration

---
+++ Backend/lms-liquibase/src/test/java/com/pxp/lmsliquibase/LmsLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pxp.lmsliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
