7686fff47ffb679a9e40c40b125e141d3986f299
Sarthak Mehta <sarthakmehta649@gmail.com>
2023-03-29 15:03:21 +0530

    filesaver dependency

--- Frontend/frontend-1/src/views/components/admin/dashboard/template/HolidayCalendar.js
+++ Frontend/frontend-1/src/views/components/admin/dashboard/template/HolidayCalendar.js
@@ -5,11 +5,13 @@ import { apiConfig } from '../../../../../api/api.config';
 import axios from "axios";
 import FileSaver from 'file-saver';
 import Swal from 'sweetalert2'
+import './template.css';
 
 export default function HolidayCalendar(props) {
 
     const [selectedFile, setSelectedFile] = useState(null);
     const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);
 
     function handleDownloadHolidayCalendar(event) {
         event.preventDefault();
@@ -18,9 +20,12 @@ export default function HolidayCalendar(props) {
 
     const onFileChange = event => {
         setSelectedFile(event.target.files[0]);
+        setMessage("");
     };
 
     const onFileUpload = (event) => {
+        setMessage("");
+        setLoading(true);
         event.preventDefault();
         const formData = new FormData();
         formData.append(
@@ -31,24 +36,45 @@ export default function HolidayCalendar(props) {
         const url = apiConfig.uploadHolidayCalendar + '/Hyderabad/2023';
         axios.post(url, formData).then((response) => {
             console.log(response);
-            Swal.fire(
-                'Success!',
-                response.data,
-                'success'
-              )
+            setLoading(false);
+            successTrigger(response.data);
         }).catch((response) => {
             console.error("Could not upload the holiday calendar excel report.", response);
+            setLoading(false);
+            failureTrigger("Could not upload the holiday calendar excel report. Error Code : "+response.code)
         });
     };
 
     const downloadSampleFile = ()=> { 
+        setMessage("");
+        setLoading(true);
         console.log("downloading sample excel...")
         axios.get(apiConfig.downloadBlankTemplate, {responseType: 'blob'}).then((response) => {
             FileSaver.saveAs(response.data, "Holiday_calendar_template.xlsx");
+            setLoading(false);
+            successTrigger("Holiday_calendar_template.xlsx Downloaded Successfully");
         }).catch((response) => {
             console.error("Could not download the excel report from the backend.", response);
+            setLoading(false);
+            failureTrigger("Could not download the holiday calendar sample excel file. Error code : "+response.code)
         });
     }
+
+    const successTrigger = (message)=> {
+         Swal.fire(
+            'Success!',
+            message,
+            'success'
+        )
+    }
+    
+    const failureTrigger = (message)=> {
+        Swal.fire(
+           'Failed!',
+           message,
+           'error'
+       )
+   }
 
 	return (<>
 		<div className="container-fluid">
@@ -84,10 +110,17 @@ export default function HolidayCalendar(props) {
                         </div>
                         <div className="mt-3"> <input type="file" onChange={onFileChange}/></div>
                         
+
                         <div style={{textAlign:"center", marginTop:"15px"}}>
+                            {loading?<div className="loader"></div>:''}
                             <div><span className="text-warning" style={{fontFamily: "monospace", fontSize:"14px"}}>{message}</span></div>
                             <input className="btn btn-success m-2" type="submit" value="Upload" disabled={selectedFile==null}/>
-                            <button className="btn btn-danger m-2" onClick={close}>Cancel</button>
+                            <button className="btn btn-danger m-2" onClick={()=>{
+                                close(); 
+                                setMessage('')
+                                setLoading(false)
+                                setSelectedFile(null);
+                            }}>Cancel</button>
                         </div>
                         
                     </form>
