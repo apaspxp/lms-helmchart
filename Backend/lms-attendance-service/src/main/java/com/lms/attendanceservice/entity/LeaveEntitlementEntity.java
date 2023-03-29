1a407bc87c02b6a74519d61da5da01bf2eafed8d
Sarthak Mehta <sarthakmehta649@gmail.com>
2023-03-29 22:54:22 +0530

    LMS-10 : attendance service

---
+++ Backend/lms-attendance-service/src/main/java/com/lms/attendanceservice/entity/LeaveEntitlementEntity.java
@@ -0,0 +1,16 @@
+package com.lms.attendanceservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Table(name="LEAVE_ENTITLEMENT")
+public class LeaveEntitlementEntity {
+
+}
