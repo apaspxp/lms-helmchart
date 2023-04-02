2f5689ea265ddba1ae04b79efce4cab2febd187b
rajesh1420621 <panigrahirajesh578@gmail.com>
2023-04-03 00:41:00 +0530

    LMS-12: Create a skeleton for Email Service (spring cloud stream).

---
+++ Backend/lms-email-service/src/main/java/com/pxp/lmsemailservice/LmsEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pxp.lmsemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsEmailServiceApplication.class, args);
+	}
+
+}
---
+++ Backend/lms-email-service/src/test/java/com/pxp/lmsemailservice/LmsEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pxp.lmsemailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
