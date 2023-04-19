2ac732d9501f5fa52e39a8f83fbca8f2676e54ca
Sarthak Mehta <sarthakmehta649@gmail.com>
2023-04-19 17:38:48 +0530

    LMS-15 : setup skeleton for profile service

--- Backend/lms-liquibase/src/main/java/com/pxp/lmsliquibase/config/DatasrourceConfiguration.java
+++ Backend/lms-liquibase/src/main/java/com/pxp/lmsliquibase/config/DatasrourceConfiguration.java
@@ -12,16 +12,12 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatasrourceConfiguration {
+
+    //Leave
     @Bean
     @Primary
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource primaryDataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.attendance-datasource")
-    public DataSource secondaryDataSource() {
         return DataSourceBuilder.create().build();
     }
 
@@ -36,6 +32,13 @@ public class DatasrourceConfiguration {
         return springLiquibase(primaryDataSource(), primaryLiquibaseProperties());
     }
 
+    //Attendance
+    @Bean
+    @ConfigurationProperties(prefix = "spring.attendance-datasource")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
     @Bean
     @ConfigurationProperties(prefix = "spring.attendance-liquibase")
     public LiquibaseProperties secondaryLiquibaseProperties() {
@@ -46,6 +49,25 @@ public class DatasrourceConfiguration {
     public SpringLiquibase secondaryLiquibase() {
         return springLiquibase(secondaryDataSource(), secondaryLiquibaseProperties());
     }
+
+    //Profile
+    @Bean
+    @ConfigurationProperties(prefix = "spring.profile-datasource")
+    public DataSource lmsProfileServiceDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.profile-liquibase")
+    public LiquibaseProperties lmsProfileServiceLiquibaseProperties() {
+        return new LiquibaseProperties();
+    }
+
+    @Bean
+    public SpringLiquibase lmsProfileServiceLiquibase() {
+        return springLiquibase(lmsProfileServiceDataSource(), lmsProfileServiceLiquibaseProperties());
+    }
+
 
     private static SpringLiquibase springLiquibase(DataSource dataSource, LiquibaseProperties properties) {
         SpringLiquibase liquibase = new SpringLiquibase();
---
+++ Backend/lms-liquibase/src/main/resources/lms_profile_service_db.changelog/dml/dml_1.sql
@@ -0,0 +1,5 @@
+-- liquibase formatted sql
+
+-- changeset Sarthak:1681902748664-1
+-- preconditions onFail:MARK_RAN onError:MARK_RAN
+-- precondition-sql-check expectedResult:0 SELECT count(*) FROM employee WHERE employee_id = 1;
