630eb01cb3718a9fea0c469ed4d924148d580bed
Sarthak Mehta <sarthakmehta649@gmail.com>
2023-05-01 15:37:42 +0530

    LMS-24 : Create an Endpoint for  : Swipe

--- Backend/lms-attendance-service/src/main/java/com/lms/attendanceservice/entity/AttendanceEntity.java
+++ Backend/lms-attendance-service/src/main/java/com/lms/attendanceservice/entity/AttendanceEntity.java
@@ -1,28 +1,48 @@
 package com.lms.attendanceservice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.Date;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="ATTENDANCE")
+@IdClass(AttendanceId.class)
 public class AttendanceEntity {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int employeeId;
+    @Column(name = "EMPLOYEE_ID")
+    private String employeeId;
+
+    @Id
+    @Column(name = "DATE")
     private Date date;
-    private Date inTime;
-    private Date outTime;
-    private String duration;
-    private String attendanceRecord;
+
+    @Column(name = "IN_TIME")
+    private Time inTime;
+
+    @Column(name = "OUT_TIME")
+    private Time outTime;
+
+    @Column(name = "DURATION")
+    private Long duration;
+
+    @Column(name = "ATTENDANCE_RECORD_TYPE")
+    private String attendanceRecordType;
+
+    @Column(name = "APPROVAL_STATUS")
     private String approvalStatus;
+
+    @Column(name = "APPROVED_BY")
     private String approvedBy;
+
+    @Column(name = "COMMENTS")
     private String comments;
 }
